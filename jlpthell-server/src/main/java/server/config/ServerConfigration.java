fc01e70238bf544bf0dd950c324d5364f9e7ac1f
Seungyeop Lee <xbeast7@gmail.com>
2019-07-02 23:44:40 +0900

    내장 DB 설정 수정
    불필요한 설정 부분(중복 설정 부분) 수정
    shutdown로직 추가 (AbstractEmbeddedDatabaseConfigurer에서 사용하는 로직)
    sql파일에 OracleSql 호환 모드를 나타내는 sql문 제거

--- jlpthell-server/src/main/java/server/config/ServerConfigration.java
+++ jlpthell-server/src/main/java/server/config/ServerConfigration.java
@@ -1,8 +1,6 @@
 package server.config;
 
-import javax.sql.DataSource;
-
-import org.hsqldb.jdbc.JDBCDriver;
+import org.hsqldb.jdbcDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,56 +10,67 @@ import org.springframework.jdbc.datasource.embedded.ConnectionProperties;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseConfigurer;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import server.ServerMain;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Configuration
 @ComponentScan(basePackageClasses = ServerMain.class)
 @EnableTransactionManagement
 public class ServerConfigration {
-	
-	@Bean
-	public PlatformTransactionManager transactionManager() {
-		return new DataSourceTransactionManager(embeddedDatabase());
-	}
 
-	/**
-	 * 내장 데이터베이스의 DataSource
-	 */
-	@Bean
-	public EmbeddedDatabase embeddedDatabase() {
-		EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
-		factory.setDatabaseConfigurer(databaseConfigurer());
-		factory.setDatabaseType(EmbeddedDatabaseType.HSQL);
-		factory.setDatabasePopulator(populator());
-		return factory.getDatabase();
-	}
-	
-	@Bean
-	public EmbeddedDatabaseConfigurer databaseConfigurer() {
-		return new EmbeddedDatabaseConfigurer() {
-			@Override
-			public void configureConnectionProperties(ConnectionProperties properties, String databaseName) {
-				properties.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_ora=true");
-				properties.setUsername("sa");
-				properties.setPassword("");
-				properties.setDriverClass(JDBCDriver.class);
-			}
-			@Override
-			public void shutdown(DataSource dataSource, String databaseName) {}
-		};
-	}
-	
-	@Bean
-	public DatabasePopulator populator() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new DefaultResourceLoader().getResource("sql.sql"));
-		return populator;
-	}
-	
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(embeddedDatabase());
+    }
+
+    /**
+     * 내장 데이터베이스의 DataSource
+     */
+    @Bean
+    public EmbeddedDatabase embeddedDatabase() {
+        EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
+        factory.setDatabaseConfigurer(databaseConfigurer());
+        factory.setDatabasePopulator(populator());
+        return factory.getDatabase();
+    }
+
+    @Bean
+    public EmbeddedDatabaseConfigurer databaseConfigurer() {
+        return new EmbeddedDatabaseConfigurer() {
+            @Override
+            public void configureConnectionProperties(ConnectionProperties properties, String databaseName) {
+                properties.setUrl("jdbc:hsqldb:mem:testdb;sql.syntax_ora=true");
+                properties.setUsername("sa");
+                properties.setPassword("");
+                properties.setDriverClass(jdbcDriver.class);
+            }
+
+            @Override
+            public void shutdown(DataSource dataSource, String databaseName) {
+                //AbstractEmbeddedDatabaseConfigurer의 shutdown메소드 로직
+                try {
+                    Connection connection = dataSource.getConnection();
+                    Statement stmt = connection.createStatement();
+                    stmt.execute("SHUTDOWN");
+                } catch (SQLException ex) {
+                }
+            }
+        };
+    }
+
+    @Bean
+    public DatabasePopulator populator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new DefaultResourceLoader().getResource("sql.sql"));
+        return populator;
+    }
+
 }
